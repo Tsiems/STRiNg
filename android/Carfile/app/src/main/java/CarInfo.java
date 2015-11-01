67cc0572571c6cc001c6d256b2cef123807022e0
nickr77 <nickroberts57@gmail.com>
2015-11-01 13:30:35 -0600

    Added settings activity, not implemented yet

---
+++ android/Carfile/app/src/main/java/CarInfo.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Nick on 11/1/2015.
+ */
+public class CarInfo {
+    private String carName;
+    private String make;
+    private String model;
+    private int year;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
